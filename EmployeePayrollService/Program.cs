52e5dac4a81ba95f73cd3caa39416d069a0c45cd
Prashantbhure50 <prashantbhure50@gmail.com>
2021-05-25 17:47:58 +0530

    Add project files.

---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll!");
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeName = "Mohan";
+            employee.BasicPay = 10000.00M;
+            employee.StartDate = Convert.ToDateTime("2020-11-03");
+            employee.Gender = 'M';
+            employee.PhoneNumber = "6302907678";
+            employee.Address = "02-Patna";
+            employee.Department = "Tech1";
+            employee.Deductions = 1500.00;
+            employee.TaxablePay = 23444.00;
+            employee.Tax = 4000.00;
+            employee.NetPay = 700000.00;
+            employee.area=2223.33;
+           
+
+            if (repo.AddEmployee(employee))
+                Console.WriteLine("Records added successfully");
+           // repo.GetAllEmployee();
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ EmployeePayrollTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeePayrollTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeePayrollTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06f0989e-746b-49d9-b1e2-40c9ab7a0ca1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
